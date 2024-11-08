e82dda497661bdd0eb7fff653e07bbf5bf5febe7
soIipsist <solipsist14@gmail.com>
2024-11-08 21:02:08 +0200

    fetch file info from manifest.db

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import os
 import shutil
 import subprocess
 import shlex
-
+from enum import Enum
 BACKUP_DIRECTORY = "~/Library/Application Support/MobileSync/Backup/"
 BACKUP_DIRECTORY = os.path.expanduser(BACKUP_DIRECTORY)
 
+class Domain(str, Enum):
+    FILES = "AppDomainGroup-group.com.apple.FileProvider.LocalStorage"
+    # MUSIC = ""
+    # MOVIES = ""
+    # BOOKS = ""
+    PHOTOS = "CameraRollDomain"
+    
 class File():
     fileID = None
     domain = None
@@ -15,13 +22,36 @@ class File():
     flags = None
     file = None
     
+    extension = None
+    size = None
+    file_name = None
+    
     def __init__(self, fileID, domain, relativePath,flags, file ) -> None:
         self.fileID = fileID
         self.domain = domain 
         self.relativePath = relativePath
         self.flags = flags
         self.file = file
+    
+    def move_file(self, destination_directory:str):
+        pass
+    
+    @property
+    def size(self):
+        return len(self.file) if self.file else 0
 
+    @property
+    def extension(self):
+        return os.path.splitext(self.relativePath)[1][1:]
+    
+    @property
+    def file_name(self):
+        return os.path.basename(self.relativePath)
+    
+    def __repr__(self) -> str:
+        return f"<File fileID={self.fileID}, name={self.file_name}"
+    
+    
 def get_default_backup_directory():
     default_backup_directory = os.path.join(os.getcwd(), "backup")
     default_backup_directory = os.path.join(default_backup_directory, os.listdir(default_backup_directory)[0])
@@ -54,13 +84,18 @@ def extract_media(backup_directory:str, destination_directory:str = None, extens
         print(e)
         
     files = map_sqlite_results_to_objects(results, File)
-    print(len(files), vars(files[-1]))
+    
+    for file in files:
+        print(file)
     
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-b", "--backup_directory", type=str, default=get_default_backup_directory())
 parser.add_argument("-d", "--destination_directory", type=str, default=None)
+parser.add_argument("-d", "--domains", type=list, default=[Domain.FILES])
 parser.add_argument("-e", "--extensions", type=list)
+parser.add_argument("-r", "--remove_backup", type=bool,default=False)
+
 args = vars(parser.parse_args())
 
 extract_media(**args)
