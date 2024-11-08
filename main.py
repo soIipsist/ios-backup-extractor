e9d2e4e36844f81ffd87006fb8f9caa873ce158d
soIipsist <solipsist14@gmail.com>
2024-11-08 21:54:36 +0200

    updated list of domains

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ BACKUP_DIRECTORY = "~/Library/Application Support/MobileSync/Backup/"
 BACKUP_DIRECTORY = os.path.expanduser(BACKUP_DIRECTORY)
 
 class Domain(str, Enum):
+    ALL = "all"
     FILES = "AppDomainGroup-group.com.apple.FileProvider.LocalStorage"
-    # MUSIC = ""
-    # MOVIES = ""
-    # BOOKS = ""
+    MEDIA = "MediaDomain"
+    BOOKS = "AppDomain-com.apple.iBooks"
     PHOTOS = "CameraRollDomain"
     
 class File():
@@ -33,9 +33,25 @@ class File():
         self.flags = flags
         self.file = file
     
-    def move_file(self, destination_directory:str):
-        pass
-    
+    def move_file(self, backup_directory:str, output_directory:str):
+        os.makedirs(output_directory, exist_ok=True)
+        
+        sub_directory = self.fileID[:2]
+        
+        source_path = os.path.abspath(os.path.join(backup_directory, sub_directory, self.relativePath))
+        destination_path = os.path.abspath(os.path.join(output_directory, self.relativePath))
+        
+        print(source_path, destination_path)
+        return
+        try:
+            if os.path.exists(source_path):  # Check if source file exists
+                subprocess.run(["cp", source_path, destination_path], check=True)
+                print(f"Copied: {self.relativePath}")
+            else:
+                print(f"Source file not found: {self.source_path}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error copying {self.source_path} to {destination_path}: {e}")
+        
     @property
     def size(self):
         return len(self.file) if self.file else 0
@@ -63,7 +79,35 @@ def map_sqlite_results_to_objects(
 ):
     return [object_type(*row) for row in sqlite_results]
 
-def extract_media(backup_directory:str, destination_directory:str = None, extensions:list = []):
+def get_files(database: str, domains: list = None, extensions: list = None):
+    conn = sqlite3.connect(database)
+    
+    if domains:
+        placeholders = ','.join(['?'] * len(domains))
+        query = f"SELECT * FROM Files WHERE domain IN ({placeholders})"
+        params = domains
+    else:
+        query = "SELECT * FROM Files"
+        params = []
+    
+    try:
+        cursor = conn.execute(query, params)
+        results = cursor.fetchall()
+    except Exception as e:
+        print(e)
+        return []
+
+    files = [
+        file for file in map_sqlite_results_to_objects(results, File)
+        if not extensions or file.extension in extensions
+    ]
+    
+    return files
+def extract_media(backup_directory:str, output_directory:str = None, domains:list = None,extensions:list = None, remove_backup:bool = False):
+    
+    if domains and Domain.ALL in domains:
+        domains = None
+        
     if not os.path.isdir(backup_directory): # this is not a valid director, so it must be a name
         backup_directory = os.path.join(BACKUP_DIRECTORY,backup_directory)
     
@@ -73,27 +117,23 @@ def extract_media(backup_directory:str, destination_directory:str = None, extens
     if not os.path.exists(database):
         raise FileNotFoundError("Manifest.db file not found.")
     
-    conn = sqlite3.connect(database)
-
-    query = "SELECT * FROM Files WHERE domain = 'AppDomainGroup-group.com.apple.FileProvider.LocalStorage'"
-    results = []
-    try:
-        cursor = conn.execute(query)
-        results = cursor.fetchall()
-    except Exception as e:
-        print(e)
-        
-    files = map_sqlite_results_to_objects(results, File)
+    files = get_files(database, domains, extensions)
     
     for file in files:
-        print(file)
+        file.move_file(backup_directory,output_directory)
     
+    if remove_backup:
+        os.rmdir(backup_directory)
+
+
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-b", "--backup_directory", type=str, default=get_default_backup_directory())
-parser.add_argument("-d", "--destination_directory", type=str, default=None)
-parser.add_argument("-d", "--domains", type=list, default=[Domain.FILES])
-parser.add_argument("-e", "--extensions", type=list)
+parser.add_argument("-o", "--output_directory", type=str, default=desktop_path) # moves to desktop directory by default
+parser.add_argument("-d", "--domains", nargs="?", default=[Domain.FILES])
+parser.add_argument("-e", "--extensions", nargs="+")
 parser.add_argument("-r", "--remove_backup", type=bool,default=False)
 
 args = vars(parser.parse_args())
